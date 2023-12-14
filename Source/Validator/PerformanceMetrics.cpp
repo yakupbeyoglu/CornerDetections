060ba0792e32d798242140df0eb261366584f062
yakupbeyoglu <yakupbeyoglu@gmail.com>
2023-12-14 02:33:25 +0100

    feat: Metrics structure added with new operators  and adapt to calculate functions

--- Source/Validator/PerformanceMetrics.cpp
+++ Source/Validator/PerformanceMetrics.cpp
@@ -5,16 +5,33 @@
 #include "../Processing/Common.h"
 
 namespace Validator {
-std::vector<std::tuple<std::string, double>> GeneralMetrics::CalculateMetrics(
-    /*
-     * ng = number of ground-truth corners(Real corners)
-     * nt = number of detected corners after transformation
-     * no = nnumber of detected corners on the input image before transformation
-     */
+Metrics GeneralMetrics::CalculateMetrics(
+
     Types::PolygonMap original_map, Types::PolygonMap predicted_map) {
   if (original_map.size() != predicted_map.size())
     throw std::runtime_error(
         "Original polygon size is not matched with predicted !");
+
+  ResetMetrics();
+
+  for (auto original = std::begin(original_map),
+            predicted = std::begin(predicted_map);
+       original != std::end(original_map); ++original, ++predicted) {
+    avg_metrics += CalculateMetric(original->second, predicted->second, 1.5);
+  }
+
+  avg_metrics /= original_map.size();
+  return avg_metrics;
+}
+
+Metrics GeneralMetrics::CalculateMetric(const Types::CvPointList original_list,
+                                        Types::CvPointList predicted_list,
+                                        double th) {
+  /*
+   * ng = number of ground-truth corners(Real corners)
+   * nt = number of detected corners after transformation
+   * no = nnumber of detected corners on the input image before transformation
+   */
   auto get_precision = [](const int &nt, const int &nr) {
     return nt > 0 ? static_cast<double>(nr) / nt : 0.0;
   };
@@ -37,18 +54,52 @@ std::vector<std::tuple<std::string, double>> GeneralMetrics::CalculateMetrics(
     return 100 * (static_cast<double>(nr) / (nt + no));
   };
 
-  ResetMetrics();
+  Metrics metrics;
+  // number of groundtruth(correct corners)
+  int ng = original_list.size();
 
-  for (auto original = std::begin(original_map),
-            predicted = std::begin(predicted_map);
-       original != std::end(original_map); ++original, ++predicted) {
+  // number of detected corner
+  int nt = predicted_list.size();
+
+  // before transformation
+  int no = original_list.size();
+
+  // number of true corners among the detected corners
+  int nr = 0;
+
+  for (const auto &original : original_list) {
+    std::size_t found_index;
+    cv::Point matched =
+        FindMatchedPoint(original, predicted_list, found_index, th);
+    if (matched.x != -1 && matched.y != -1) {
+      ++nr;
+      predicted_list.erase(std::begin(predicted_list) + found_index);
+#ifdef VERBOSE
+      std::cout << "Original Point: (" << original
+                << ") matches with Found Point: (" << matched << ")"
+                << std::endl;
+#endif
+    } else {
+#ifdef VERBOSE
+      std::cout << "Original Point : (" << original << ") has no match"
+                << std::endl;
+#endif
+    }
   }
+
+  metrics.precision = get_precision(nt, nr);
+  metrics.recall = get_recall(nr, ng);
+  metrics.apr = get_apr(metrics.precision, metrics.recall);
+  metrics.fscore = get_fscore(metrics.precision, metrics.recall);
+  metrics.ar = get_ar(nr, nt, no);
+
+  return metrics;
 }
 
-cv::Point GeneralMetrics::FindMatchedPoints(cv::Point original_point,
-                                            const Types::CvPointList predicted,
-                                            std::size_t &found_index,
-                                            double th) {
+cv::Point GeneralMetrics::FindMatchedPoint(cv::Point original_point,
+                                           const Types::CvPointList predicted,
+                                           std::size_t &found_index,
+                                           double th) {
   cv::Point matched = {-1, -1};
   double min_distance = std::numeric_limits<double>::max();
   for (std::size_t i = 0; i < predicted.size(); ++i) {
--- Source/Validator/PerformanceMetrics.h
+++ Source/Validator/PerformanceMetrics.h
@@ -14,40 +14,66 @@
  * @see https://github.com/yakupbeyoglu
  */
 namespace Validator {
+struct Metrics {
+  double precision;
+  double recall;
+  double apr;
+  double fscore;
+  double ar;
+
+  Metrics &operator/=(const int &divisor) {
+    if (divisor == 0)
+      throw std::runtime_error("Divisor can not be less than 0!");
+    precision /= divisor;
+    recall /= divisor;
+    apr /= divisor;
+    fscore /= divisor;
+    ar /= divisor;
+    return *this;
+  }
+
+  Metrics &operator+=(const Metrics &other) {
+    precision += other.precision;
+    recall += other.recall;
+    apr += other.apr;
+    fscore += other.fscore;
+    ar += other.ar;
+    return *this;
+  }
+};
+
+template <class P_ = cv::Point>
 
 class PerformanceMetrics {
  public:
-  std::vector<std::tuple<std::string, double>> void CalculateMetrics(
-      Types::PolygonMap original_map, Types::PolygonMap predicted_map) = 0;
+  virtual Metrics CalculateMetrics(Types::PolygonMap original_map,
+                                   Types::PolygonMap predicted_map) = 0;
   virtual void ExportMetricsToCSV() = 0;
 };
 
 class GeneralMetrics : public PerformanceMetrics {
  public:
-  virtual std::vector<std::tuple<std::string, double>> CalculateMetrics(
-      Types::PolygonMap original_map,
-
-      Types::PolygonMap predicted_map) override;
+  virtual Metrics CalculateMetrics(Types::PolygonMap original_map,
+                                   Types::PolygonMap predicted_map) override;
   virtual void ExportMetricsToCSV() override;
 
  private:
   void ResetMetrics() {
-    avg_precision = 0;
-    avg_recall = 0;
-    avg_apr = 0;
-    avg_fscore = 0;
-    avg_ar = 0;
+    avg_metrics.precision = 0;
+    avg_metrics.recall = 0;
+    avg_metrics.apr = 0;
+    avg_metrics.fscore = 0;
+    avg_metrics.ar = 0;
+    avg_metrics.nr = 0;
+    avg_metrics.
   }
 
-  cv::Point FindMatchedPoints(cv::Point original_point,
-                              const Types::CvPointList,
-                              std::size_t &found_index, double th = 1.5);
+  Metrics CalculateMetric(const Types::CvPointList original_list,
+                          Types::CvPointList predicted_list, double th = 1.5);
+  cv::Point FindMatchedPoint(cv::Point original_point, const Types::CvPointList,
+                             std::size_t &found_index, double th = 1.5);
 
-  double avg_precision;
-  double avg_recall;
-  double avg_apr;
-  double avg_fscore;
-  double avg_ar;
+  Metrics avg_metrics;
 };
 }  // namespace Validator
 
